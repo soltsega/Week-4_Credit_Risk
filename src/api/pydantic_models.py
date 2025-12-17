98cf32868872687f83805244984f774710fa6e10
soltsega <tsegasolomon538@gmail.com>
2025-12-17 22:28:55 +0300

    pydantic file

--- src/api/pydantic_models.py
+++ src/api/pydantic_models.py
@@ -1,3 +1,26 @@
-# unit tests
-import pytest
-from pydantic import BaseModel, ValidationError, conint, constr 
+from pydantic import BaseModel
+from typing import Optional
+
+class ModelInput(BaseModel):
+    TransactionId: str
+    BatchId: str
+    AccountId: str
+    SubscriptionId: str
+    CustomerId: str
+    CurrencyCode: str
+    CountryCode: int
+    ProviderId: str
+    ProductId: str
+    ProductCategory: str
+    ChannelId: str
+    Amount: float
+    Value: float
+    TransactionStartTime: str
+    PricingStrategy: int
+    FraudResult: int
+
+class ModelOutput(BaseModel):
+    transaction_id: str
+    risk_probability: float
+    risk_category: str
+    confidence: Optional[float] = None
